ac885408b05d230228f342f10a3476b121d4bd3d
Erick Vargas <erickkvargas@gmail.com>
2022-06-10 09:24:26 -0700

    commit tree 24978dc2c4238f4f8d62f300895166e18cf2b1e1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # if we want to create our own routes we can use this format 
+  # get "/spices", to: "spices#index"
+  # get "/spices/:id", to: "spices#show"
+
+    # for specific CRUD routes we can just do this
+   resources :spices, only: [:index, :create, :update, :destroy]
+
+  #if were going to use all our crud routes we can just do 
+
+  # resources :spices 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
